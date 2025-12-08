b90e1717f1ed950491b26fa87ae112c87083f300
Git Kurs User <leandrokuenstner@gmail.com>
2025-12-08 13:41:46 +0100

    Interfaces hinzugefügt

--- src/Interfaces/Observer.java
+++ src/Interfaces/Observer.java
@@ -1,4 +1,6 @@
 package Interfaces;
 
 public interface Observer {
+
+    void update(float temperatur, float luftfeuchtigkeit);
 }
--- src/Interfaces/Subject.java
+++ src/Interfaces/Subject.java
@@ -1,4 +1,9 @@
 package Interfaces;
 
 public interface Subject {
+    void hinzufuegen(Observer beobachter);
+
+    void entfernen(Observer beobachter);
+
+    void benachrichtigen();
 }
